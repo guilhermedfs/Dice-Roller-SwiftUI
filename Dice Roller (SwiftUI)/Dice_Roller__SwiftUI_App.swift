1eea76df4343db066382cf28f0741dab9a62eca5
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-12-15 09:54:53 -0300

    Initial Commit

---
+++ Dice Roller (SwiftUI)/Dice_Roller__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_Roller__SwiftUI_App.swift
+//  Dice Roller (SwiftUI)
+//
+//  Created by Guilherme Fernandes - pessoal on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Dice_Roller__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
